ee4f3f4f20f63de3c5aa548ff68b0b01531f8bf8
Armel <captainnyobe@gmail.com>
2020-05-19 13:34:07 +0100

    dynamic array

---
+++ dynamic-array/src/main/java/DynamicArray.java
@@ -0,0 +1,228 @@
+/**
+ * Data Structures
+ * Dynamic Array (basics operations implementation)
+ * @author kenany (armelknyobe@gmail.com)
+ *
+ */
+public class DynamicArray {
+
+	private static final int INITIAL_CAPACITY = 16;
+	// The number of existing items
+	private int size = 0;
+	// The number of items the dynamic array can contains
+	private int capacity;
+	
+	private Object values[];
+	
+	
+	public DynamicArray() {
+		this.capacity = INITIAL_CAPACITY;
+		this.values = new Object[this.capacity];
+	}
+	
+	public int length() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return length() == 0;
+	}
+	
+	/**
+	 * O(n)
+	 * Add item to the dynamic array
+	 * @param value item to add
+	 * @return true
+	 */
+	public boolean add(Object value) {
+		
+		if (size == capacity) {
+			capacity *= 2;
+			Object twice[] = new Object[capacity];
+			
+			for (int i = 0; i < size; i++) {
+				twice[i] = values[i];
+			}
+			
+			values = twice;
+		}
+		
+		values[size++] = value;
+		
+		return true;
+		
+	}
+	
+	/**
+	 * O(n*m) n = size of dynamic, m = size of array to add
+	 * Add many items to the dynamic array
+	 * @param array Array to items to add
+	 * @return true
+	 */
+	public boolean add(Object array[]) {
+		
+		for (Object object : array) {
+			add(object);
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * O(1)
+	 * Retrieve item from index in dynamic array; 
+	 * Throw IndexOutOfBoundsException if dynamic array size is less or equal than the provided index
+	 * @param index
+	 * @return
+	 */
+	public Object get(int index) {
+		
+		if (index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		return values[index];
+	}
+	
+	/**
+	 * O(1)
+	 * Update item if it exists in dynamic array; 
+	 * Throw IndexOutOfBoundsException if dynamic array size is less or equal than the provided index
+	 * @param index Index of item to update
+	 * @param value The new value
+	 * @return true or throw IndexOutOfBoundsException
+	 */
+	public boolean update(int index, Object value) {
+		
+		if (index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		values[index] = value;
+		
+		return true;
+	}
+	
+	/**
+	 * O(n)
+	 * Delete item if it exists in dynamic array; 
+	 * Throw IndexOutOfBoundsException if dynamic array size is less or equal than the provided index
+	 * @param index Index of item to delete
+	 * @return the deleted item or throw IndexOutOfBoundsException
+	 */
+	public Object delete(int index) {
+		
+		if (index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		Object value = values[index];
+		
+		Object newArray[] = new Object[capacity];
+		
+		int j = 0;
+		
+		for (int i = 0; i < size; i++) {
+			
+			if (i != index) {
+				newArray[j] = values[i];
+				j++;
+			}
+		}
+		
+		values = newArray;
+		size--;
+		
+		return value;
+	}
+	
+	/**
+	 * O(n)
+	 * Retrieve index of the given value
+	 * @param value Value for which we find the index
+	 * @return the value's index or -1 if It doesn't exists
+	 */
+	public int indexOf(Object value) {
+		
+		for (int i = 0; i < size; i++) {
+			
+			if (values[i].equals(value)) {
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+	
+	/**
+	 * Remove all items from dynamic array
+	 */
+	public void clear() {
+		capacity = INITIAL_CAPACITY;
+		values = new Object[capacity];
+		size = 0;
+	}
+	
+	/**
+	 * O(n) n = end - start + 1
+	 * Seek the dynamic array from start index to end index
+	 * Throw IllegalArgumentException if start index is greater than end index
+	 * Throw IndexOutOfBoundsException if start index is less than 0 or end index is greater than dynamic array size
+	 * @param start Index to start
+	 * @param end Index to end (It is really end - 1)
+	 * @return new dynamic array containing items coming from this one taken at start index to end index
+	 */
+	public DynamicArray seek(int start, int end) {
+		
+		if (end < start) {
+			throw new IllegalArgumentException("the start index can't be greater than end index");
+		}
+		
+		if (start < 0 || end >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		Object part[] = new Object[end - start + 1];
+		int index = 0;
+		
+		for (int i = start; i <= end; i++) {
+			part[index++] = values[i];
+		}
+		
+		DynamicArray seeker = new DynamicArray();
+		
+		seeker.add(part);
+		
+		return seeker;
+	}
+	
+	/**
+	 * O(n) n = size - start
+	 * Seek this dynamic array from start index to the last item
+	 * @param start Index to start
+	 * @return new dynamic array containing items comming from this one taken at start index to the last item
+	 */
+	public DynamicArray subList(int start) {
+		
+		if (start >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		return seek(start, size -1);
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder asString = new StringBuilder("[");
+		
+		for (int i = 0; i < size; i++) {
+			asString.append(values[i].toString() + ",");
+		}
+		
+		asString.append("]");
+		
+		return asString.toString();
+	}
+	
+}
---
+++ dynamic-array/src/test/java/DynamicArrayTest.java
@@ -0,0 +1,244 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DynamicArrayTest {
+
+    DynamicArray dynamicArray;
+    DynamicArray dynamicArray2;
+    Character[] characters;
+
+    @Before
+    public void setUp() throws Exception {
+        dynamicArray = new DynamicArray();
+        dynamicArray2 = new DynamicArray();
+        dynamicArray2.add("foo");
+
+        characters = new Character[97];
+
+        for (int i = 0; i < 97; i++) {
+            char toAdd = (char)(' ' + i);
+            characters[i] = toAdd;
+        }
+    }
+
+    @Test
+    public void length() {
+        int firstSize = dynamicArray.length();
+
+        assertEquals("The first Dynamic array size equals to 0", firstSize, 0);
+
+        int secondSize = dynamicArray2.length();
+
+        assertEquals("The second Dynamic array size equals to 1", secondSize, 1);
+    }
+
+    @Test
+    public void isEmpty() {
+
+        assertTrue("The ffirst Dynamic array is empty", dynamicArray.isEmpty());
+
+        assertFalse("The second Dynamic array isn't empty", dynamicArray2.isEmpty());
+    }
+
+    @Test
+    public void add() {
+        int size = dynamicArray.length();
+        dynamicArray.add("foo");
+        int newSize = dynamicArray.length();
+
+        assertEquals("The new size equals to the first size increased to 1", newSize, size + 1);
+
+        for (int i = 0; i < 15; i++) {
+            dynamicArray.add("" + i);
+        }
+
+        size = dynamicArray.length();
+
+        assertEquals("The size of dynamic array equals to 16", size, 16);
+    }
+
+    // Add array
+    @Test
+    public void testAdd() {
+
+        int size = dynamicArray.length();
+
+        int arraySize = characters.length;
+
+        dynamicArray.add(characters);
+
+        int newSize = dynamicArray.length();
+
+        assertEquals("The dynamic array size has increased of size of array", newSize, size + arraySize);
+    }
+
+    @Test
+    public void get() {
+
+        String foo = "foo";
+
+        boolean isEmpty = dynamicArray.isEmpty();
+
+        assertTrue("The dynamic array is empty", isEmpty);
+
+        assertThrows("Cannot get item from empty dynamic array; should throw exception", IndexOutOfBoundsException.class, () -> {
+            dynamicArray.get(0);
+        });
+
+        final int testIndex = 1;
+        int index = 31;
+
+        dynamicArray.add(foo);
+
+        String item = (String)dynamicArray.get(0);
+
+        int size = dynamicArray.length();
+
+        assertEquals("The retrieved item equals to the added item", item, foo);
+
+        assertEquals("Index equals to size of dynamic array", testIndex, size);
+
+        assertThrows("Cannot get item whose the position is greater or equals than the Dynamic array size; should throw exception", IndexOutOfBoundsException.class, () -> {
+            dynamicArray.get(testIndex);
+        });
+
+        dynamicArray.add(characters);
+
+        Character retrieved = (Character)dynamicArray.get(index);
+
+        Character expected = characters[index - 1];
+
+        assertEquals("The retrieved item equals to expected item", retrieved, expected);
+
+        size = dynamicArray.length();
+
+        final int newIndex = size + 10;
+
+        assertThrows("Cannot get item whose the position is greater or equals than the Dynamic array size; should throw exception", IndexOutOfBoundsException.class, () -> {
+            dynamicArray.get(newIndex);
+        });
+    }
+
+    @Test
+    public void update() {
+
+        String toAdd = "foo";
+        final String toUpdate = "bar";
+
+        dynamicArray.add(toAdd);
+
+        String retrieved = (String)dynamicArray.get(0);
+
+        assertEquals("The retrieved item equals to added item", retrieved, toAdd);
+
+        dynamicArray.update(0, toUpdate);
+
+        retrieved = (String)dynamicArray.get(0);
+
+        assertEquals("The retrieved item equals to item used for update", retrieved, toUpdate);
+
+        assertThrows("Cannot update item with unexisting index; should throw exception", IndexOutOfBoundsException.class, () -> {
+            dynamicArray.update(5, toUpdate);
+        });
+    }
+
+    @Test
+    public void delete() {
+
+        assertThrows("Cannot delete item on an empty Dynamic array; should throw exception", IndexOutOfBoundsException.class, () -> {
+            dynamicArray.delete(0);
+        });
+
+        String foo = "foo";
+
+        int index = 0;
+
+        dynamicArray.add(foo);
+        dynamicArray.add(characters);
+
+        int sizeBeforeDelete = dynamicArray.length();
+
+        String retrievedItem = (String) dynamicArray.get(index);
+
+        String deleted = (String)dynamicArray.delete(index);
+
+        int sizeAfterDelete = dynamicArray.length();
+
+        assertEquals("The Dynamic array size before delete equals to its size after delete + 1", sizeBeforeDelete, sizeAfterDelete + 1);
+
+        assertEquals("The deleted item equals to retrieved item at the same index", deleted, retrievedItem);
+    }
+
+    @Test
+    public void indexOf() {
+
+        String foo = "foo";
+        String bar = "bar";
+
+        dynamicArray.add(characters);
+        dynamicArray.add(foo);
+
+        int index = dynamicArray.indexOf(foo);
+
+        assertTrue("The retrieved index exists, that means it is a positive value", index > -1);
+
+        String retrived = (String)dynamicArray.get(index);
+
+        assertSame("The item retrieved at the index is same to item used to get this given index", retrived, foo);
+
+        index = dynamicArray.indexOf(bar);
+
+        assertEquals("This item doesn't exists so the returned index equals to -1", index, -1);
+    }
+
+    @Test
+    public void clear() {
+
+        dynamicArray.add(characters);
+
+        assertFalse("The Dynamic array it isn't empty", dynamicArray.isEmpty());
+
+        dynamicArray.clear();
+
+        assertTrue("The Dynamic array becomes empty", dynamicArray.isEmpty());
+    }
+
+    @Test
+    public void seek() {
+
+        dynamicArray.add(characters);
+
+        dynamicArray2.clear();
+
+        int start = 10;
+        int end = 20;
+        final int size = dynamicArray.length();
+
+        assertThrows("The start position can't be greater than the end position", IllegalArgumentException.class, () -> {
+            int begin = 30;
+            int stop = 10;
+
+            dynamicArray.seek(begin, stop);
+        });
+
+        assertThrows("The start position can't be less than 0", IndexOutOfBoundsException.class, () -> {
+            dynamicArray.seek(-1, 20);
+        });
+
+        assertThrows("The end position can't be greater or equals than the Dynamic array size", IndexOutOfBoundsException.class, () -> {
+            dynamicArray.seek(10, size);
+        });
+
+        dynamicArray2 = dynamicArray.seek(start, end);
+
+        int limit = end - start + 1;
+
+        assertEquals("The new dynamic array size equals to end - start + 1", dynamicArray2.length(), limit);
+
+        for (int i = 0; i < limit; i++) {
+            assertEquals("The retrieved items from differents Dynamic arrays are equals", dynamicArray2.get(i), dynamicArray.get(start + i));
+        }
+    }
+
+}
